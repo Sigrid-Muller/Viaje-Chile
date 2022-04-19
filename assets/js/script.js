70561782ff99216ec617726d5f4b0fd02819d34c
Sigrid <oasismuller@gmail.com>
2022-04-18 21:58:47 -0400

    se sube el proyecto

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,9 +6,18 @@ $(document).ready(function(){
         $("html, body").animate(
             {
 
-                scrollTop: $(gato).offset().top -70
+                scrollTop: $(gato).offset().top -10
             },
             800
         )
     })
+})
+
+$('#enviarcorreo').click(function(){
+    alert("El correo fue enviado correctamente...");
+  });
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
 })
